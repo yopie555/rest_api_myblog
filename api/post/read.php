5be32df5e145048c0f0d196a7f91bbc4bf95ac93
Yopy Tri Buana <nero555@Yopys-MacBook-Air.local>
2023-07-25 22:28:31 +0700

    p8.1

--- api/post/read.php
+++ api/post/read.php
@@ -1,49 +1,48 @@
 <?php
 //headers
-header('Access-Control-Allow-Origin: *'); //public
-header('Content-Type: application/json'); //json
+header('Access-Control-Allow-Origin: *'); //public api
+header('Content-Type: application/json'); //json data
 
 include_once '../../config/Database.php'; //database connection
 include_once '../../models/Post.php'; //post model
 
 //Instantiate DB & connect
-$database = new Database(); //create database object
-$db = $database->connect(); //call connect method
+$database = new Database();
+$db = $database->connect();
 
 //Instantiate blog post object
-$post = new Post($db); //create post object
+$post = new Post($db);
 
 //Blog post query
-$result = $post->read(); //call read method
-
+$result = $post->read();
 //Get row count
-$num = $result->rowCount(); //get row count
+$num = $result->rowCount();
 
 //Check if any posts
 if ($num > 0) {
     //Post array
-    $posts_arr = array();
-    $posts_arr['data'] = array();
+    $posts_arr = array(); //this is the same as $posts_arr = [];
+    $posts_arr['data'] = array(); //this is the same as $posts_arr['data'] = [];
 
-    while ($row = $result->fetch(PDO::FETCH_ASSOC)) { //fetch associative array
-        extract($row); //extract row
-
-        $post_item = array( //create post item
-            'id' => $id,
-            'title' => $title,
-            'body' => html_entity_decode($body), //decode html entities
-            'author' => $author,
-            'category_id' => $category_id,
-            'category_name' => $category_name
+    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+        extract($row);
+        //this is not needed because 
+        //we are not using extract() in the model
+        //this is the same as extract($row) 
+        //but we are not using extract() in the model
+        $post_item = array(
+            'id' => $row['id'],
+            'title' => $row['title'],
+            'body' => html_entity_decode($row['body']), //html_entity_decode() is used to decode html entities
+            'author' => $row['author'],
+            'category_id' => $row['category_id'],
+            'category_name' => $row['category_name']
         );
+
         //Push to "data"
-        array_push($posts_arr['data'], $post_item);
+        array_push($posts_arr['data'], $post_item); //this is the same as $posts_arr['data'][] = $post_item;
     }
+
     //Turn to JSON & output
     echo json_encode($posts_arr);
-} else {
-    //No posts
-    echo json_encode(
-        array('message' => 'No Posts Found')
-    );
 }
